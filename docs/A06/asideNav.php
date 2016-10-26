c365d4d7c7d2852171c3ddfc236c4fef241cfe26
Michael Schmid <schmm11@bfh.ch>
2016-10-26 08:39:13 +0200

    Added A06

---
+++ docs/A06/asideNav.php
@@ -0,0 +1,20 @@
+<?php
+echo '
+    <aside>
+        <div id="search"> 
+            <form>
+                <input type="text" name="search"> <input type="submit" value="S">
+            </form>
+        </div>
+
+    <div id="asideNav">
+        <ul>
+            <li> Bestseller </li>
+            <li> Releases </li>
+            <li> Budget </li>
+        </ul>
+    </div>
+    
+    </aside>
+'
+?>
---
+++ docs/A06/functions.php
@@ -0,0 +1,64 @@
+<?php
+function get_param($name, $default) {
+	if (!isset($_GET[$name]))
+	return $default;
+	return urldecode($_GET[$name]);
+}
+
+
+function add_param($url, $name, $value) {
+	if(strpos($url, '?') !== false) $sep = '&';
+	else $sep = '?';
+	return $url . $sep . $name . "=" . urlencode($value);
+}
+
+function asideNavigation($language, $pageId) {
+	$urlbase = add_param( $_SERVER['PHP_SELF'], "lang", $language);
+	for ($i = 0; $i <= 5; $i++) {
+	$url = add_param( $urlbase, "id", $i);
+	$class = $pageId == $i ? 'active' : 'inactive';
+	echo '<li><a class="'.$class.'" href="'.$url.'">'.t('page')." $i</a></li>";
+	}
+}
+
+function languages($language, $pageId) {
+	$languages = array('de','fr', 'en');
+	$urlbase = add_param($_SERVER['PHP_SELF'], 'id', $pageId);
+	foreach( $languages as $l) {
+		$class = $language == $l ? 'active' : 'inactive';
+		echo '<a class="'.$class.'" href="';
+		echo add_param($urlbase,'lang', $l).'">';
+		echo strtoupper($l).'</a>';
+	}
+}
+
+function t($key) {
+	global $language;
+	$texts = array(
+	'page' => array(
+	'de'=>'Seite',
+	'fr'=>'Page',
+	'en'=>'Page' ),
+	'content' => array(
+	'de'=>'Willkommen auf der Seite ',
+	'fr'=>'Bienvenue à la page ',
+	'en'=>'Welcome to the page '));
+	return isset($texts[$key][$language])
+	? $texts[$key][$language]
+	: "[$key]";
+}
+
+function content($pageId) {
+
+	if( $pageId == 11){
+			include 'ProductBuy.php';
+	}
+	else if ($pageId == 1) 
+	{
+			include 'bestseller.php';
+	}
+	else {
+	echo t('content') . " $pageId";
+	}
+}
+?>
