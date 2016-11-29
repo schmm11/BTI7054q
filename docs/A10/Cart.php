21f4f0c3b5e3fd971e8a9b40442a1cdc8109926d
Michael Schmid <schmm11@bfh.ch>
2016-11-29 18:37:40 +0100

    added Cart

--- docs/A10/Cart.php
+++ docs/A10/Cart.php
@@ -12,24 +12,38 @@ function __construct(){
 
 public function removeItem($item, $num) {
 	if (!isset($this->items[$item])) return;
-	$this->items[$item] -= $num;
+	$this->items[$itemitem] -= $num;
 	if ($this->items[$item] <= 0)
 	unset($this->items[$item]);
 }
 public function getItems() {
 	return $this->items;
 }
-public function isEmpty() {
-	return count($this->items) == 0;
-}
 
 
 
 public function addItem($item, $num) {
-	if (!isset($this->items[$item]))
-	$this->items[$item] = 0;
+	if (!isset($this->items[$item])){
+		$this->items[$item] = 0;
+	}
 	$this->items[$item] += $num;
 }
 
+public function isEmpty() {
+	return count($this->items) == 0;
+}
+public function countItem(){
+	return count($this->items);
+}
+
+public function render(){
+	if ($this->isEmpty()) {
+		echo "Empty Cart";
+		return;
+	}
+	echo "Has Items...";
+	
+	
+}
 
 }
